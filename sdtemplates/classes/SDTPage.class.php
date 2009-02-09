9965c2c062337c1f89970b6f470e94856311713f
marevalo <marevalo@d65ef8c0-f555-11dd-99c0-75d572ba1ddd>
2009-02-09 20:38:00 +0000

    Make SDTPage a subclass of SDTNode

--- sdtemplates/classes/SDTPage.class.php
+++ sdtemplates/classes/SDTPage.class.php
@@ -1,9 +1,8 @@
 <?php
 include_once( "SDTNode.class.php" );
 
-class SDTPage {
-	private $DOMDocument ;
-	private $sourceFile ;
+class SDTPage extends SDTNode {
+	protected $DOMDocument ;
 	
 	function __construct ( $filename , $DOMDocument = NULL ) {
 		$this->sourceFile = $filename ;
@@ -13,6 +12,7 @@ class SDTPage {
 		} else {
 			$this->DOMDocument = $DOMDocument ;
 		}
+		parent::__construct( $this , $this->DOMDocument , $this->sourceFile );
 		/* After this we should process all relative URI attrs
 		 * for TAGS (HTML4.01):
 		 * action FORM
@@ -41,59 +41,6 @@ class SDTPage {
 		 */
 	}
 	
-	function getSourceFile ( ) {
-		return $sourceFile ;
-	}
-	
-	function getFirstNodeByTagName ( $tagName ) {
-		$nodeList = $this->DOMDocument->getElementsByTagName( $tagName ) ;
-		if ( $nodeList->length != 0 ) {
-			return new SDTNode ( 
-						$this ,
-						$nodeList->item(0)
-					);
-			
-		}
-	}
-	
-	function getNodeById ( $id ) {
-		return new SDTNode (
-						$this ,
-						$this->DOMDocument->getElementById( $id )
-					);
-	}
-	
-	function getNodeByClass ( $class , $node = NULL ) {
-		if ( is_null($node) ) {
-			$node = $this->DOMDocument;
-		}
-		if ( $node->hasAttributes() ) {
-			$attributes = $node->attributes;
-			$Nattributes = $attributes->length ;
-			for ( $index = 0 ; $index < $Nattributes ; $index++ ) {
-				if ( strtoupper( $attributes->item ( $index )->name ) 
-						== "CLASS" &&
-				     $attributes->item ( $index )->value == $class ) {
-					return new SDTNode ( $this , $node );
-				}
-			}
-		}
-		if ( $node->hasChildNodes() ) {
-			$mylist = $node->childNodes;
-			$Nnodes = $mylist->length;
-			for ( $index = 0 ; $index < $Nnodes ; $index++ ) {
-				$result =  $this->getNodeByClass (
-								$class ,
-								$mylist->item ( $index )
-							);
-				if ( !is_null($result) ) {
-					return $result;
-				}
-			}
-		}
-		return NULL;
-	}
-	
 	function clonePage ( ) {
 		return NULL;
 	}
@@ -104,42 +51,6 @@ class SDTPage {
 
 	function printHTML () {
 		print $this->DOMDocument->saveHTML();
-	}
-	
-	function walkThrough ( $tab = "\t" , $prefix = "" , $node = NULL ) {
-		$return_string = "";
-		if ( is_null($node) ) {
-			$node = $this->DOMDocument;
-		}
-		$return_string .= "$prefix NAME , TYPE : ".$node->nodeName." , ".
-			$node->nodeType."\n";
-		if ( $node->nodeType == XML_TEXT_NODE ) {
-			$return_string .= "$prefix VALUE: ".$node->nodeValue."\n";
-		}
-		if ( $node->hasAttributes() ) {
-			$attributes = $node->attributes;
-			$Nattrs = $attributes->length;
-			$return_string .= "$prefix $Nattrs attributes.\n";
-			for ( $index = 0 ; $index < $Nattrs ; $index++ ) {
-				$return_string .=
-					$prefix." ".$attributes->item ( $index )->name.
-					": ".$attributes->item ( $index )->value."\n";
-			}
-		}
-		if ( $node->hasChildNodes() ) {
-			$mylist = $node->childNodes;
-			$Nnodes = $mylist->length;
-			$return_string .= "$prefix $Nnodes nodes.\n";
-			for ( $index = 0 ; $index < $Nnodes ; $index++ ) {
-				$subnode = $mylist->item ( $index );
-				$return_string .= $this->walkThrough(
-										$tab ,
-										$prefix.$tab ,
-										$subnode
-									);
-			}
-		}
-		return $return_string;
 	}
 	
 }
